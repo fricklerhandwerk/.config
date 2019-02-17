a921aac50c084a736ef9f7baff0009db0b0909a7
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-02-17 12:23:19 +0100

    use home-manager as system package and wrap per user
    this creates a base state for user configuration on system rebuild, and
    copies the configuration used into user's `$HOME` if there is none on
    each call to `home-manager`.
    
    unfortunately the user still has to call `home-manager` at least once to
    get `.config` into its home directory, but this may be omitted if
    copying is not done in a wrapper but a user service instead.

--- configuration.nix
+++ configuration.nix
@@ -1,27 +1,31 @@
-{ config, pkgs, ... }:
-  let home-manager = pkgs.home-manager.overrideAttrs ( oldAttrs:{
-    src = pkgs.fetchFromGitHub {
-      owner = "fricklerhandwerk";
-      repo = "home-manager";
-      rev = "643dff586d3e9d676571f712688563a0e5f14f97";
-      sha256 = "0jxfw1hgfiiir308pw83wsgs33wijhyj0dwm8bxviqlyrkwwzs97";
-    };
-    buildCommand = ''
-      install -v -D -m755 ${home-manager.src}/home-manager/home-manager $out/bin/home-manager
-      substituteInPlace $out/bin/home-manager \
-        --subst-var-by bash "${pkgs.bash}" \
-        --subst-var-by coreutils "${pkgs.coreutils}" \
-        --subst-var-by findutils "${pkgs.findutils}" \
-        --subst-var-by gnused "${pkgs.gnused}" \
-        --subst-var-by less "${pkgs.less}" \
-        --subst-var-by nix "${pkgs.nix}" \
-        --subst-var-by HOME_MANAGER_PATH '${home-manager.src}'
-     '';
-   }); in
+{ config, pkgs,  ... }:
+with builtins;
+with pkgs;
+let
+  hmsrc = fetchTarball https://github.com/rycee/home-manager/archive/release-18.09.tar.gz;
+  home-manager = import "${hmsrc}/home-manager" {inherit pkgs;};
+  # TODO: declare `config` option for `config.users.users.{user}` to define
+  # this where it belongs
+  vg-config = fetchGit {
+    url = "https://github.com/fricklerhandwerk/.config";
+    ref = "master";
+  };
+  # TODO: if this step is done in a user service on login, fallback frequency
+  # will still be good enough, but more importantly we can skip the initial
+  # manual call to `home-manager` currently required to get the config into
+  # user's `$HOME`
+  user-home-manager = config: writeShellScriptBin "home-manager" ''
+    if [[ ! -d "$HOME/.config" ]]; then
+      cp -r "${config}" "$HOME/.config"
+    fi
+    exec "${home-manager}" -I home-manager=${hmsrc} $@
+  '';
+in
 {
   imports =
     [
       ./hardware-configuration.nix
+      "${hmsrc}/nixos"
     ];
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
@@ -33,29 +37,28 @@
 
     environment.systemPackages = with pkgs; [
       neovim
+      git
       home-manager
     ];
 
-    # TODO: run this only for specific user
-    system.userActivationScripts = {
-      home = {
-        text = ''
-          # assuming absence of `~/.config` is equivalent to a fresh install,
-          # fetch user config from remote repository
-          if [[ ! -d ~/.config ]]; then
-            ${pkgs.git}/bin/git clone git://github.com/fricklerhandwerk/.config.git
-            ${home-manager}/bin/home-manager switch
-          fi
-        '';
-        deps = [];
-
-      };
-    };
   users.users.vg = {
     isNormalUser = true;
     uid = 1000;
     extraGroups = [ "wheel" "networkmanager" ];
+    packages = [
+      (symlinkJoin {
+        name = "home-manager";
+        paths = [
+          (user-home-manager vg-config)
+          home-manager
+        ];
+      })
+    ];
   };
+  # this creates or activates the most recent profile on login, which means
+  # that later changes to the profile by the will not be overridden by a system
+  # rebuild
+  home-manager.users.vg = import "${vg-config}/nixpkgs/home.nix";
 
   system.stateVersion = "18.09";
 }
