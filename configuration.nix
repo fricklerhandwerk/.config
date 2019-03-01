411d2d2e08ec3c92d953e541a0c12c60a2616fd5
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-03-01 17:57:18 +0100

    let user service copy config
    this does not work as intended yet. while the service description is
    copied into $HOME - which indicates that it counts as part of the user
    environment - the service itself does not run on login.
    
    maybe the service is still misconfigured. but on the other hand, if we
    copy config manually to $HOME and run `home-manager switch`, it is
    executed properly. I have to find an example service which is supposed
    to start on login and see if it really does after system-build.

--- configuration.nix
+++ configuration.nix
@@ -2,30 +2,15 @@
 with builtins;
 with pkgs;
 let
-  hmsrc = fetchTarball https://github.com/rycee/home-manager/archive/release-18.09.tar.gz;
-  home-manager = import "${hmsrc}/home-manager" {inherit pkgs;};
-  # TODO: declare `config` option for `config.users.users.{user}` to define
-  # this where it belongs
-  vg-config = fetchGit {
-    url = "https://github.com/fricklerhandwerk/.config";
-    ref = "master";
-  };
-  # TODO: if this step is done in a user service on login, fallback frequency
-  # will still be good enough, but more importantly we can skip the initial
-  # manual call to `home-manager` currently required to get the config into
-  # user's `$HOME`
-  user-home-manager = config: writeShellScriptBin "home-manager" ''
-    if [[ ! -d "$HOME/.config" ]]; then
-      cp -r "${config}" "$HOME/.config"
-    fi
-    exec "${home-manager}" -I home-manager=${hmsrc} $@
-  '';
+  home-manager = import (
+    fetchTarball https://github.com/rycee/home-manager/archive/release-18.09.tar.gz
+    ) { inherit pkgs; };
 in
 {
   imports =
     [
       ./hardware-configuration.nix
-      "${hmsrc}/nixos"
+      home-manager.nixos
     ];
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
@@ -38,27 +23,23 @@ in
     environment.systemPackages = with pkgs; [
       neovim
       git
-      home-manager
+      home-manager.home-manager
     ];
 
   users.users.vg = {
     isNormalUser = true;
     uid = 1000;
     extraGroups = [ "wheel" "networkmanager" ];
-    packages = [
-      (symlinkJoin {
-        name = "home-manager";
-        paths = [
-          (user-home-manager vg-config)
-          home-manager
-        ];
-      })
-    ];
   };
   # this creates or activates the most recent profile on login, which means
   # that later changes to the profile by the will not be overridden by a system
   # rebuild
-  home-manager.users.vg = import "${vg-config}/nixpkgs/home.nix";
+  home-manager.users.vg =
+    let config = fetchGit {
+      url = "https://github.com/fricklerhandwerk/.config";
+      ref = "master";
+    };
+    in (import config) "${config}";
 
   system.stateVersion = "18.09";
 }
