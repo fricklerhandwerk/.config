86d3d2b72c277c3007a8b4cbf76f676e3e5ce517
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-03-25 02:31:14 +0100

    add setup for secrets
    the general idea is that for initial setup of the system (or user
    account) a known storage medium has to be mounted off which we can copy
    secrets into a location outside of the store.
    
    a more automated setup would only copy a single gpg keyset with
    a human-memorizable password, to use with `gpg-agent` and `ssh` for
    fetching all the other secrets off some private (or locally hosted) git
    repo. but I first learned of this possibility today and it requires some
    work to wire everything together.

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1,8 +1,14 @@
 { pkgs, ... }:
+with builtins;
+with lib;
+with import <home-manager/modules/lib/dag.nix> {inherit lib;};
+let secrets = "$HOME/.config/secrets"; in
 {
   home.packages = with pkgs; [
     dmenu
     qutebrowser
+    pass
+    gnupg
   ];
   xsession = {
     enable = true;
@@ -21,4 +27,24 @@
       '';
     };
   };
+  services.gpg-agent.enable = true;
+  # get secrets in place
+  home.activation.copySecrets = dagEntryAfter ["writeBoundary"] ''
+    if [[ ! -d ${secrets} ]]; then
+      mnt=$(lsblk -no MOUNTPOINT $(blkid -U 5FA0-D2A4))
+      if [[ -z $mnt ]]; then
+        echo "secret storage not mounted"
+        exit 1
+      fi
+      cp -R $mnt/secrets ${secrets}
+      chmod -R u=rwx,g=,o= ${secrets}
+      fi
+  '';
+  home.activation.sshKeys = dagEntryAfter ["copySecrets"] ''
+    install -D -m600 ${secrets}/ssh/id_rsa_github $HOME/.ssh/id_rsa_github
+  '';
+  home.activation.passwords = dagEntryAfter ["copySecrets"] ''
+    cp -R ${secrets}/password-store $HOME/.password-store
+    chmod -R u=rwx,g=,o= $HOME/.password-store
+  '';
 }
