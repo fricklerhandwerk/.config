b9b782bc321b48eb7bf122026ada3ace8b875a75
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-06-24 23:58:44 +0200

    mount secret storage before copying

--- home-manager/nixos/secrets.nix
+++ home-manager/nixos/secrets.nix
@@ -1,6 +1,9 @@
 { pkgs, lib, ... }:
 with import <home-manager/modules/lib/dag.nix> {inherit lib;};
-let secrets = "$HOME/.config/secrets"; in
+let
+  device-id = "5FA0-D2A4";
+  secrets = "$HOME/.config/secrets";
+in
 {
   home.packages = with pkgs; [
     pass
@@ -11,9 +14,20 @@ let secrets = "$HOME/.config/secrets"; in
   # get secrets in place
   home.activation.copySecrets = dagEntryAfter ["writeBoundary"] ''
     if [[ ! -d ${secrets} ]]; then
-      mnt=$(lsblk -no MOUNTPOINT $(blkid -U 5FA0-D2A4))
+      dev=$(blkid -U ${device-id})
+      if [[ -z $dev ]]; then
+        echo "secret storage not attached"
+        exit 1
+      fi
+      mnt=$(lsblk -no MOUNTPOINT $dev)
       if [[ -z $mnt ]]; then
-        echo "secret storage not mounted"
+        # "mounted <DBus device> on <mountpoint>"
+        msg=$(${pkgs.udiskie}/bin/udiskie-mount $dev)
+        mnt=$(echo $msg | rev | cut -d' ' -f1 | rev)
+      fi
+      if [[ ! -d $(realpath $mnt) ]]; then
+        echo "could not mount secret storage"
+        echo $msg
         exit 1
       fi
       cp -RT $mnt/secrets ${secrets}
