cac518b8e0aa613d0dfb5c5153f7c5def20f221b
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-06-27 00:49:26 +0200

    allow `udisks2` to mount filesystems from `systemd` unit
    seems like one has to explicitly allow udisks2 to mount file systems
    from other "seats", in this case `systmed`:
    <https://github.com/coldfix/udiskie/issues/165#issuecomment-429568341>
    
    this is a working but incomplete solution, as the permissions to
    automatically mount a filesystem should be narrowed down to what is
    absolutely required to do the job. probably this will stick for a while
    nonetheless, because having a working system and more features is more
    important than fixing up these issues.

--- home-manager/nixos/secrets.nix
+++ home-manager/nixos/secrets.nix
@@ -27,16 +27,23 @@ in
       fi
       if [[ ! -d $(realpath $mnt) ]]; then
         echo "could not mount secret storage"
-        echo $msg
         exit 1
       fi
       cp -RT $mnt/secrets ${secrets}
       chmod -R u=rwx,g=,o= ${secrets}
       fi
+      umount $mnt
   '';
+  # TODO: copy PGP keys
   home.activation.sshKeys = dagEntryAfter ["copySecrets"] ''
     install -D -m600 ${secrets}/ssh/id_rsa_github $HOME/.ssh/id_rsa_github
   '';
+  # TODO: fetch `.password-store` from a private git repo to be able to keep it
+  # up to date. PGP and SSH keys (assuming we are not using gpg-agent for SSH
+  # authentication) should be the only secrets to copy. not sure yet whether it
+  # is a good idea to have so many repositories, but so far machines, user
+  # config and secrets are three independent concepts where it makes sense to
+  # keep separate histories for.
   home.activation.passwords = dagEntryAfter ["copySecrets"] ''
     cp -RT ${secrets}/password-store $HOME/.password-store
     chmod -R u=rwx,g=,o= $HOME/.password-store
