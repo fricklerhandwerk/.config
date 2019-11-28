15fd02b00adc4864e66b3b8a6e91749796c70997
fricklerhandwerk <valentin@fricklerhandwerk.de>
2019-11-28 14:27:48 +0100

    add prototype service for fetching password-store

--- modules/linux/secrets.nix
+++ modules/linux/secrets.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 let
   # TODO: use WWN if possible (probably needs a more modern USB stick)
   device-id = "5FA0-D2A4";
@@ -63,4 +63,45 @@ with config.lib;
     cp -RT ${secrets}/password-store ${env.PASSWORD_STORE_DIR}
     chmod -R u=rwx,g=,o= ${env.PASSWORD_STORE_DIR}
   '';
+
+  systemd.user.services.password-store = {
+    Unit = {
+      Description = "Fetch password-store";
+      # XXX: the directory itself should be enough if we could detect failures
+      # and clean up properly, but `git-remote-gcrypt` swallows the error that
+      # might happen when internally re-fetching the repository, for reasons
+      # I do not understand. therefore check against an artifact of successful
+      # decryption of the repository..
+      ConditionPathExists = "!${env.PASSWORD_STORE_DIR}/.gpg-id";
+      Before = [ "default.target" ];
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+
+    Service = {
+      Type = "oneshot";
+      Environment = with pkgs; builtins.concatStringsSep " " [
+       "PATH=${lib.makeBinPath [ git gitAndTools.gitRemoteGcrypt coreutils ]}"
+       "GNUPGHOME=${env.GNUPGHOME}"
+      ];
+      ExecStart = let
+        # TODO: `git clone` still fails here [1]. apparently internal `git
+        # fetch` fails, but since it is silenced with `-q` and the surrounding
+        # function returns 0 in this case there is no direct way to find out
+        # what exactly went wrong. this is a known issue [2]. have to replace
+        # `git-remote-gcrypt` with a fork which is more verbose. probably
+        # something is missing in this service's environment, as the problem
+        # does not come up in the regular shell.
+        # [1]: https://github.com/spwhitton/git-remote-gcrypt/blob/master/git-remote-gcrypt#L540
+        # [2]: https://github.com/spwhitton/git-remote-gcrypt/blob/master/README.rst#known-issues
+        script = pkgs.writeShellScriptBin "fetch-password-store" ''
+          mkdir -p ${env.PASSWORD_STORE_DIR}
+          git clone gcrypt::https://github.com/fricklerhandwerk/password-store ${env.PASSWORD_STORE_DIR}
+          cd ${env.PASSWORD_STORE_DIR}
+          git remote set-url origin --push gcrypt::git@github.com:fricklerhandwerk/password-store
+        ''; in "${script}/bin/fetch-password-store";
+    };
+  };
 }
