d7b0dec6050b4437ded8d2d00a9f2bc702429c54
fricklerhandwerk <valentin@fricklerhandwerk.de>
2020-12-07 13:26:30 +0100

    update SSH config
    set ForwardAgent for servers to enable acting as sudo without password through SSH
    
    clean up ssh config path copypaste.

--- modules/terminal/ssh.nix
+++ modules/terminal/ssh.nix
@@ -1,24 +1,29 @@
 { config, pkgs, ... }:
+let
+  ssh = "${config.home.homeDirectory}/.ssh";
+in
 {
   programs.ssh = {
     enable = true;
     matchBlocks = {
       "github github.com" = {
         hostname = "github.com";
-        identityFile = "${config.home.homeDirectory}/.ssh/github";
+        identityFile = "${ssh}/github";
       };
-      "fricklerhandwerk fricklerhandwerk.de" = {
-        hostname = "fricklerhandwerk.de";
-        identityFile = "${config.home.homeDirectory}/.ssh/una";
+      una = {
+        forwardAgent = true;
+        hostname = "una.local";
+        identityFile = "${ssh}/una";
       };
       webgo = {
         user = "web196";
         hostname = "server18.webgo24.de";
-        identityFile = "${config.home.homeDirectory}/.ssh/una";
+        identityFile = "${ssh}/una";
       };
       ghost = {
+        forwardAgent = true;
         hostname = "89.144.19.124";
-        identityFile = "${config.home.homeDirectory}/.ssh/ghost";
+        identityFile = "${ssh}/ghost";
       };
     };
     extraConfig = ''
